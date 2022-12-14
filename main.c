0df86f3a46cc86e7743ed3142e3945c25d94f47b
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-12-14 22:15:22 +0100

    task 0 completed, i guess

--- main.c
+++ main.c
@@ -13,10 +13,6 @@ int main(int argc, char *argv[])
 	FILE *fp = fopen(argv[1], "r");
 	stack_t *stack = NULL;
 	char *line = NULL;
-	size_t n = 0;
-	char *opcode;
-	int i = 0;
-	unsigned int line_number =0;
 	instruction_t instructions[] = {
 		{"push", &push},
 		{"pall", &pall},
@@ -33,6 +29,48 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
+
+	parser(fp, instructions, stack);
+	free(line);
+	free_stack(stack);
+	fclose(fp);
+	return (0);
+}
+/**
+ * free_stack - frees the stack
+ *
+ * @stack: stack
+ *
+ * Return: void
+ */
+void free_stack(stack_t *stack)
+{
+	stack_t *tmp;
+
+	while (stack != NULL)
+	{
+		tmp = stack;
+		stack = stack->next;
+		free(tmp);
+	}
+}
+/**
+ * parser - reads the command line file passed
+ *
+ * @fp: file passed
+ * @instructions: opcode passed
+ * @stack: stack
+ *
+ * Return: void
+ */
+void parser(FILE *fp, instruction_t instructions[], stack_t *stack)
+{
+	char *line = NULL;
+	size_t n = 0;
+	char *opcode;
+	int i = 0;
+	unsigned int line_number = 0;
+
 	while (getline(&line, &n, fp) != -1)
 	{
 		line_number++;
@@ -56,20 +94,4 @@ int main(int argc, char *argv[])
 			exit(EXIT_FAILURE);
 		}
 	}
-free(line);
-free_stack(stack);
-fclose(fp);
-return 0;
 }
-void free_stack(stack_t *stack)
-{
-	stack_t *tmp;
-
-	while (stack != NULL)
-	{
-		tmp = stack;
-		stack = stack->next;
-		free(tmp);
-	}
-}
-
