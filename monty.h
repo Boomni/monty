560c467800ea004971a8236e0e3a443bd12cf30b
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-12-15 14:56:55 +0100

    edited push

--- monty.h
+++ monty.h
@@ -54,5 +54,5 @@ void sub(stack_t **stack, unsigned int line_number);
 void division(stack_t **stack, unsigned int line_number);
 void mul(stack_t **stack, unsigned int line_number);
 void mod(stack_t **stack, unsigned int line_number);
-
+int is_integer(char *str);
 #endif /* _MONTY_H_ */
