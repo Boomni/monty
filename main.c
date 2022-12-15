11974684e932e28a190a40dc4873e1c9235682e5
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-12-15 07:34:46 +0100

    difficult

--- main.c
+++ main.c
@@ -47,6 +47,8 @@ void free_stack(stack_t *stack)
 {
 	stack_t *tmp;
 
+	if (stack == NULL)
+		return;
 	while (stack != NULL)
 	{
 		tmp = stack;
--- pall.c
+++ pall.c
@@ -13,7 +13,7 @@ void pall(stack_t **stack, unsigned int line_number)
 	stack_t *current = *stack;
 	(void)line_number;
 
-	if (stack == NULL)
+	if (stack == NULL || *stack == NULL)
 		return;
 	while (current != NULL)
 	{
