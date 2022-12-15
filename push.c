2f2cb07c73395bf0d8a3e70ca4a2dd2391e9ca83
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-12-15 09:25:55 +0100

    yes! hurray

--- push.c
+++ push.c
@@ -15,7 +15,7 @@ void push(stack_t **stack, unsigned int line_number)
 	char *str;
 
 	str = strtok(NULL, " \n\t");
-	if (stack == NULL)
+	if (stack == NULL || str == NULL)
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
