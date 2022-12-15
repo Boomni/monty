74cbe1b690249879786b2c2490b84cfb2622e94e
Jonathan Boomni <110749205+Boomni@users.noreply.github.com>
2022-12-15 19:30:46 +0100

    Create pchar.c

---
+++ pchar.c
@@ -0,0 +1,37 @@
+#include "monty.h"
+
+
+/* The ascii value must be between these values to be valid */
+#define ASCII_MIN 0
+#define ASCII_MAX 127
+
+/**
+ * pchar -
+ *
+ * @stack: stack
+ * @line_number: line number in file
+ *
+ * Return: void
+ */
+void pchar(stack_t **stack, unsigned int line_number)
+{
+/* Check if the stack is empty */
+if (!stack)
+{
+printf("L%u: can't pchar, stack empty\n", line_number);
+exit(EXIT_FAILURE);
+}
+
+/* Get the ascii value from the top of the stack */
+int ascii = (*stack)->n;
+
+/* Check if the ascii value is within the valid range */
+if (ascii < ASCII_MIN || ascii > ASCII_MAX)
+{
+printf("L%u: can't pchar, value out of range\n", line_number);
+exit(EXIT_FAILURE);
+}
+
+/* Print the character with the given ascii value */
+printf("%c\n", (char) ascii);
+}
