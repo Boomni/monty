7041f6d076c4789136363e4a2556b834c03dd711
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-12-15 09:49:10 +0100

    oh! hurray

--- push.c
+++ push.c
@@ -14,13 +14,9 @@ void push(stack_t **stack, unsigned int line_number)
 	int value;
 	char *str;
 
-	if (stack == NULL)
-	{
-		fprintf(stderr, "L%d: usage: push integer\n", line_number);
-		exit(EXIT_FAILURE);
-	}
 	str = strtok(NULL, " \n\t");
-	if (str == NULL || !isdigit(str) == 0)
+
+	if (stack == NULL || str == NULL)
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
