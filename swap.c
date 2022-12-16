a1016e19213f8ccf0c495a835e37fa4f2121cbad
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-12-16 16:48:44 +0100

    edited push

--- swap.c
+++ swap.c
@@ -12,7 +12,7 @@ void swap(stack_t **stack, unsigned int line_number)
 {
 	int temp = (*stack)->n;
 
-	if ((*stack) == NULL || (*stack)->next == NULL)
+	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
