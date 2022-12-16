6a9342ced3be2357b34fbb1b3cd09cc8ff56fd47
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-12-16 16:52:33 +0100

    edited push

--- pop.c
+++ pop.c
@@ -12,7 +12,7 @@ void pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp;
 
-	if (stack == NULL || *stack == NULL)
+	if (*stack == NULL)
 	{
 		printf("L%u: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
