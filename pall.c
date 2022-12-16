33302dfdf6cf45fe99e2c3deded9462cc735b485
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-12-16 17:00:24 +0100

    edited push

--- pall.c
+++ pall.c
@@ -13,6 +13,8 @@ void pall(stack_t **stack, unsigned int line_number)
 	stack_t *current = *stack;
 	(void) line_number;
 
+	if (*stack == NULL)
+		return;
 	while (current != NULL)
 	{
 		 printf("%d\n", current->n);
