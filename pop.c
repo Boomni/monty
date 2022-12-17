dd199ef4112b481721998f63c934d3ce57c2d742
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-12-17 10:38:09 +0100

    edited pop.c function

--- pop.c
+++ pop.c
@@ -10,15 +10,18 @@
  */
 void pop(stack_t **stack, unsigned int line_number)
 {
-	stack_t *temp;
+	stack_t *tmp = NULL;
 
-	if (*stack == NULL)
+	if (!stack || !*stack)
 	{
-		printf("L%u: can't pop an empty stack\n", line_number);
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
-	temp = *stack;
-	*stack = temp->next;
-	free(temp);
+	tmp = (*stack)->next;
+	free(*stack);
+	*stack = tmp;
+	if (!*stack)
+		return; /* prevents errors cause next line might assign a NULL */
+	(*stack)->prev = NULL;
 }
